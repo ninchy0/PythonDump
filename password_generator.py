2e651b3ad39da0e0b08715fd8b98114d736c70c1
ninchy0 <minmar666@gmail.com>
2021-07-24 13:29:21 -0400

    CORRECTED A STUPID TYPO.

--- password_generator.py
+++ password_generator.py
@@ -3,29 +3,27 @@
 import random 
 import string
 
-def generator(length):  
-    special = ''
-    password = ''
+def generator(length):
 
+    password = ''
     characters = string.ascii_letters
     digits = string.digits
-    s_characters = ['!', '@', '#', '$', '%']
-    for i in s_characters:
-        special = special + i
-
+    special = '!@#$%'
     container = characters + digits + special
 
+    
     for i in range(length):
         password = password + random.choice(container)
+    
     print(f'Generated password = {password}')
 
 
 
-
 if __name__ == "__main__":
-    print('''Password Generator created by @ninchy0.
-Passowrd length more than 16 is good for health
 
-    ''')
+    print('Password Generator created by @ninchy0.')
+    print('Password length more 16 is GOOD FOR HEALTH.\n\n')
+    
+
     password_length = int(input('Enter the length of password: '))
     generator(password_length)
